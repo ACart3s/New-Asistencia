68343e3a7a3b69d2eee9581937e93afd0235fe6d
ACart3s <ap.garrido@duocuc.cl>
2024-11-20 09:26:08 -0300

    QRCODE SCAN

--- src/app/pages/scan-qr/scan-qr.page.ts
+++ src/app/pages/scan-qr/scan-qr.page.ts
@@ -1,18 +1,33 @@
 import { Component, OnInit } from '@angular/core';
-
-
+import { ModalController } from '@ionic/angular';
+import { BarcodeScanningModalComponent } from './barcode-scanning-modal.component';
+import { LensFacing } from '@capacitor-mlkit/barcode-scanning';
 
 @Component({
   selector: 'app-scan-qr',
   templateUrl: './scan-qr.page.html',
   styleUrls: ['./scan-qr.page.scss'],
 })
-
 export class ScanQrPage implements OnInit {
+  resultado = '';
 
-  constructor() { }
+  constructor(private modal: ModalController) {}
 
   ngOnInit() {
+    this.scanQR();
   }
 
+  async scanQR() {
+    const modal = await this.modal.create({
+      component: BarcodeScanningModalComponent,
+      cssClass: 'barcode-scanning-modal',
+      showBackdrop: false,
+      componentProps: { formats: [], LensFacing: LensFacing.Back },
+    });
+    await modal.present();
+    const { data } = await modal.onWillDismiss();
+    if (data) {
+      this.resultado = data;
+    }
+  }
 }
